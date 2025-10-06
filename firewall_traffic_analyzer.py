fbbc34ca0d73d2a7d0052a4cc8c36b0717a2aba4
Kandise-Perkins <kperkins10@fingerlakes.edu>
2025-10-06 14:48:47 -0400

    Update firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,8 +1,8 @@
 # FILE NAME - firewall_traffic_analyzer.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Kandise Perkins
+# DATE: October 6, 2025
+# BRIEF DESCRIPTION: 
 
 
 
@@ -14,15 +14,35 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
+def main():
+    firewall_traffic_analyzer()
+def firewall_traffic_analyzer():
+    print('=== Network Traffic Security Analyzer ==')
+    print()
+    number = int(input('Enter the port number (e.g., 80, 22, 443, 3389): '))
+    data_transfer = int(input('Enter the data transfer size in megabytes (MB): '))
+    print()
+    print('FIREWALL LOG:')
+    print(f'Port: {number}, Transfer Size: {data_transfer} MB')
+
+    #risk analysis
+    if (number == 22 or number == 3389) and data_transfer >=100:
+       analysis = 'HIGH RISK: Potential unauthorized remote access detected.'
+    elif number == 80 and data_transfer > 100:
+       analysis = 'MEDIUM RISK: Large unencrypted transfer detected.'
+    elif number == 443:
+        analysis = 'LOW RISK: Secure encrypted transfer detected.'
+    else:
+        analysis = 'UNKNOWN: Unrecognized traffic pattern.'
+
+    print(f'RISK ASSESSMENT:{analysis}')
+
+    print('------------------------')
+            
 
 
 
-
-
-
-
-
-
+main()
 
 ########### END YER CODE ABOVE THIS LINE ###########
 
@@ -50,10 +70,10 @@ Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.
 === Network Traffic Security Analyzer ===
 
 Enter the port number (e.g., 80, 22, 443, 3389): 22
-Enter the data transfer size in megabytes (MB): 12
+Enter the data transfer size in megabytes (MB): 1200
 
 FIREWALL LOG:
-Port: 22, Transfer Size: 12 MB
+Port: 22, Transfer Size: 1200 MB
 Risk Assessment: HIGH RISK: Potential unauthorized remote access detected!
 ------------------------
 '''
@@ -89,7 +109,7 @@ Risk Assessment: UNKNOWN: Unrecognized traffic pattern.
 '''
 
 1. Did you get tripped up using the `or` or `and` operators? If so, how?
-
+I got mixed up on the signs going the correct way, also
 
 
 
